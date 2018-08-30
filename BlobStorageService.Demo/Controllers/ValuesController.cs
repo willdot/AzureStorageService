c4d6ffd3e46cfff6cf17ceaddd13b6b6efe4a29c
Will Andrews <will7989@hotmail.com>
2018-08-30 14:18:09 +0100

    Tidied demo controller

--- BlobStorageService.Demo/Controllers/ValuesController.cs
+++ BlobStorageService.Demo/Controllers/ValuesController.cs
@@ -16,47 +16,20 @@ namespace BlobStorageService.Demo.Controllers
         {
             azureProvider = _azureProvider;
         }
-
-        // GET api/values
+        
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
 
-            Console.WriteLine(azureProvider);
-
             BlobStorageTools tools = new BlobStorageTools(azureProvider);
 
-            //tools.Upload("sample-de542a93-8a61-4ba6-b2e5-ed3e76f0c56c", @"c:\temp\Upload.txt");
+            tools.Upload("sample-de542a93-8a61-4ba6-b2e5-ed3e76f0c56c", @"c:\temp\Upload.txt");
 
             tools.Download("sample-de542a93-8a61-4ba6-b2e5-ed3e76f0c56c", @"c:\temp\", "Test.txt");
 
 
-            return new string[] { "value1", "value2" };
+            return Ok();
         }
 
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
     }
 }
